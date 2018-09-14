57367cc7589cc6026972e99aeac0b34e4401e327
Nick Kasten <kastencode@gmail.com>
2018-09-13 23:08:30 -0500

    image segment buttons with some basic styling in studio

--- src/components/ImageStudio/index.js
+++ src/components/ImageStudio/index.js
@@ -4,15 +4,24 @@ import { getFormattedName } from '../../utils'
 
 const generateStudioImage = (image, num) => {
   return (
-    <div className={`studioImage ${ num }`}>
-      <h3>
+    <div className={`panel panel-default studioImage ${ num }`}>
+      <h3 className='studioImageTitle'>
         { getFormattedName(image) }
       </h3>
       <img 
-        className="studioImage"
+        className="studioImageThumb"
         src={ image.segments.source.url }
         alt={ image.id }
       />
+      <div className="ObjBtnContainer">
+        { Object.keys(image.segments).map(seg => {
+          return (
+            <button className="studioObjBtn btn btn-default">
+            {seg}
+            </button>
+          ) }) 
+        }
+      </div>
     </div> 
   )
 }
@@ -33,7 +42,7 @@ const ImageStudio = props => {
 
         { 
           props.images.two ? 
-          generateStudioImage(props.images.two, 'two')
+            generateStudioImage(props.images.two, 'two')
           : 
             <span /> 
         }
