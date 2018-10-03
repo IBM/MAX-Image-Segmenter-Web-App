8b472153c1485062cf46e6d3a08d5c15ea6153e7
Nick Kasten <kastentx@gmail.com>
2018-10-03 12:27:27 -0500

    looks good except for unnecessary re-render

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Particles from 'react-particles-js'
 import '../styles/AppHeader.css'
 
 const AppHeader = () => (
   <div className="titleBanner">
+  <Particles
+    className='particles'
+    params={particleParams} />
     <span className="titleText">
       <u><b>MAX</b> Image Segmenter</u>  Magic Cropping Tool
     </span>
@@ -10,3 +14,76 @@ const AppHeader = () => (
 )
 
 export default AppHeader
+
+
+const particleParams = {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#00aaaa"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#00aaaa",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "retina_detect": true
+}
