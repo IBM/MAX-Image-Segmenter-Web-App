a708b3a2a17826710f857efe60a07dab5ff797d9
Nick Kasten <kastentx@gmail.com>
2018-10-19 13:09:23 -0500

    whitespace

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-
 import { Grid, Row, Col } from 'react-bootstrap'
 import AppHeader from './components/AppHeader'
 import UploadForm from './components/UploadForm'
